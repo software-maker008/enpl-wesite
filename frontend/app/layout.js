62e601ad370d764677a2c75e99d04779fc2f4b3b
software-maker008 <software@ehsnetworks.in>
2026-01-07 17:40:57 +0530

    Update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,6 @@
 import { Jost } from "next/font/google";
 import "./globals.css";
+import FloatingTicket from "@/components/FloatingTicket";
 
 const jost = Jost({ subsets: ["latin"] });
 
@@ -7,13 +8,13 @@ export const metadata = {
   title: "EHS Networks",
   description: "EHS Networks",
 };
-  
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={jost.className + " min-h-screen flex flex-col bg-white"}>
+      <body className={`${jost.className} min-h-screen flex flex-col bg-white`}>
         {children}
+        <FloatingTicket />
       </body>
     </html>
   );
